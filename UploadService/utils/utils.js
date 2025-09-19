f5a5a429793581f94c252de3a8794be061b3e823
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-09-20 00:47:31 +0530

    Add initial UploadService setup with package.json and utility functions
    - Created package.json for UploadService with dependencies including aws-sdk, express, and redis.
    - Implemented utility functions in utils.js:
      - getAllFiles: Recursively retrieves all file paths from a specified directory, excluding .git folders.
      - generate: Generates a random string of 5 characters from a predefined subset.

---
+++ UploadService/utils/utils.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+import path from "path";
+
+export const getAllFiles = (folderPath) => {
+    let response = [];
+
+    const allFilesAndFolders = fs.readdirSync(folderPath);
+    allFilesAndFolders.forEach(file => {
+        // Skip .git folders
+        if (file === '.git') {
+            return; // Skip this iteration
+        }
+
+        const fullFilePath = path.join(folderPath, file);
+        if (fs.statSync(fullFilePath).isDirectory()) {
+            response = response.concat(getAllFiles(fullFilePath))
+        } else {
+            response.push(fullFilePath);
+        }
+    });
+    return response;
+}
+
+
+console.log(getAllFiles("../output"))
+
+
+export function generate() {
+    const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
+    const length = 5;
+    let id = "";
+    for (let i = 0; i < length; i++) {
+        id += subset[Math.floor(Math.random() * subset.length)];
+    }
+    return id;
+} 
